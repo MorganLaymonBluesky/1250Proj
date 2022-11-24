086101f56141f63eeeea208d6e221268c41972cc
MorganLaymonBluesky <113386451+MorganLaymonBluesky@users.noreply.github.com>
2022-11-24 18:53:48 -0500

    Add files via upload

---
+++ Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI1250_FinalProject
+{
+    internal class Course
+    {
+        string courseName;
+        int courseNumber;
+
+        public Course()
+        {
+            this.courseName = "XXXX";
+            this.courseNumber = 0;
+        }
+        public Course(string courseName, int courseNumber)
+        {
+            this.courseName = courseName;
+            this.courseNumber = courseNumber;
+        }
+        public override string ToString()
+        {
+            string msg = "";
+            msg += $"{courseName} {courseNumber}";
+            return msg;
+        }
+    }
+}
